5f01341a5e139601759454609901870a1fefdf0c
Nikolai Perlt <nikolaiperlt@gmail.com>
2022-02-13 22:53:01 +0100

    stuff

--- lua/_cmp.lua
+++ lua/_cmp.lua
@@ -1,4 +1,5 @@
 local cmp = require "cmp"
+local lspkind = require "lspkind"
 local luasnip = require "luasnip"
 
 cmp.setup {
@@ -40,7 +41,20 @@ cmp.setup {
     sources = {
         {name = "nvim_lsp"},
         {name = "luasnip"},
-        {name = "buffer", keyword_length = 5},
+        {name = "buffer", keyword_length = 5}
+    },
+    formatting = {
+        format = lspkind.cmp_format(
+            {
+                mode = "symbol_text", -- show only symbol annotations
+                maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                -- The function below will be called before any actual modifications from lspkind
+                -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                before = function(entry, vim_item)
+                    return vim_item
+                end
+            }
+        )
     }
 }
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
